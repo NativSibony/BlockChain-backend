5105ebe787a67da1f155c53fc0488f6e5192e2c2
nativsibony <nativs@nucleon.sh>
2021-07-07 15:22:34 +0300

    enable cors

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const port = process.env.PORT || 3000;
 const controller = require("./utils/controller");
 const app = express();
+
+app.use(cors());
 
 app.get("/block", (req, res) => {
   let ans = controller.blockCall(1, "01/01/1970", {}, 1);
