d21f033268b541065d82de6f35fd8389462abc09
nativsibony <nativs@nucleon.sh>
2021-07-07 11:03:39 +0300

    .

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 app.get("/hash", (req, res) => {
-  res.render("hash");
+  res.send("Hello World");
 });
 
 app.get("*", (req, res) => {
