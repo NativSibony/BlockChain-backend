4119068edc9e4dd75a83937cf1683181603fbccb
nativsibony <nativs@nucleon.sh>
2021-07-09 12:26:24 +0300

    .

--- src/app.js
+++ src/app.js
@@ -8,7 +8,9 @@ app.use(cors());
 
 app.get("/block", (req, res) => {
   const blockNum = parseInt(req.query.num);
-  const blockData = JSON.parse(req.query.data);
+  const blockData = JSON.parse(req.query.data)
+    ? JSON.parse(req.query.data)
+    : "";
   const ans = controller.blockCall(blockNum, "", blockData, blockNum);
 
   res.send(JSON.parse(ans));
